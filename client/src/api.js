6787b55aafccaf946363a08a2241d12f0f733c33
VIJAY <vajjavijay625@gmail.com>
2025-11-19 22:30:10 +0530

    changed

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api'
+  baseURL: process.env.REACT_APP_API_URL || 'chat-q6p0pg07g-nitins-projects-a2f51245.vercel.app'
 });
 
 export function setAuthToken(token) {
